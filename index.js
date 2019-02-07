1d342cf6459a809579dfbb937e59546ee6ac7da4
Mark Karavan <mkaravan@Marks-MBP.home>
2019-02-06 20:54:09 -0500

    seems to work so far

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hello");
+var node = function (value, lesser, greater) {
+  this.value = value;
+  this.lesser = lesser;
+  this.greater = greater;
+};
+
+var original_tree = new node(null, null, null);
+
+var stream = [3, 9, 20, 4, 16, 10, 11, 30, 50];
+
+
+
+var tree_walk = function(tree, value) {
+  if (tree == null) {
+    var new_node = new node(value, null, null);
+    return new_node;
+  } else {
+    if (value < tree.value) {
+      tree.lesser = tree_walk(tree.lesser, value);
+      return tree;
+    } else if (value > tree.value) {
+      tree.greater = tree_walk(tree.greater, value);
+      return tree;
+    }
+  }
+}
+
+var insert_next_stream_value_into_tree = function(tree, stream) {
+  if (stream.length == 0) return tree;
+
+  var value = stream.pop();
+
+  var new_tree = tree_walk(tree, value);
+
+  return insert_next_stream_value_into_tree(new_tree, stream);
+}
+
+
+console.log("BUILDING");
+var final_tree = insert_next_stream_value_into_tree(null, stream);
+
+console.log(final_tree);
